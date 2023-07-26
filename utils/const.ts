0052b01953f46e6eb5cbb1bd4b25369b76a1b906
482F <482F.git.mail@482f.net>
2023-07-27 00:00:00 +0900

    git のブランチ名の持ち方を変更

--- utils/const.ts
+++ utils/const.ts
@@ -1,6 +1,8 @@
 export const ConfigFileName = 'sync-config.json5'
 
-export const BranchNames = {
-  remote: 'sync-config---remote',
-  local: 'sync-config---local',
+export const Git = {
+  branch: {
+    remote: 'sync-config---remote',
+    local: 'sync-config---local',
+  },
 } as const
