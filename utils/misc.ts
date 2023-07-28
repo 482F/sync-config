f6e14cfdd04d87fcb987109c8e0ea6b8b9c37d27
482F <482F.git.mail@482f.net>
2023-07-29 00:00:00 +0900

    config を読む関数を追加

--- utils/misc.ts
+++ utils/misc.ts
@@ -1,13 +1,52 @@
-export type Config = {
+import { Result } from 'https://raw.githubusercontent.com/482F/482F-ts-utils/v2.x.x/src/result.ts'
+import { doExtends } from 'https://raw.githubusercontent.com/482F/482F-ts-utils/v2.x.x/src/json.ts'
+import { ConfigFileName } from './const.ts'
+import JSON5 from 'https://deno.land/x/json5@v1.0.0/mod.ts'
+
+const configModel = {
   repository: {
-    url: string
-    branch: string
-  }
-  folders: Array<{
-    name: string
-    destination: string
-  }>
+    url: '',
+    branch: '',
+  },
+  folders: [
+    {
+      name: '',
+      destination: '',
+    },
+  ],
 }
+
+export type Config = typeof configModel
+
+export async function isExists(filePath: string) {
+  return await Deno.stat(filePath).catch(() => null).then(
+    Boolean,
+  )
+}
+
+export const getConfig: () => Promise<Result<Config>> = (() => {
+  const configPromise = (async (): Promise<Result<Config>> => {
+    const configFilePath = `./${ConfigFileName}`
+    const isExistsConfig = await isExists(configFilePath)
+    if (!isExistsConfig) {
+      return [
+        undefined,
+        new ExpectedError(
+          'カレントディレクトリに sync-config.json5 が存在しません',
+        ),
+      ]
+    }
+
+    const config: unknown = JSON5.parse(await Deno.readTextFile(configFilePath))
+    const [extendedConfig, e] = doExtends(config, configModel)
+    if (e) {
+      return [undefined, e]
+    }
+
+    return [extendedConfig, undefined]
+  })()
+  return () => configPromise
+})()
 
 export class ExpectedError extends Error {
 }
