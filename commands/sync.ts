b9de39e5433047662543f17f16481249bc471c10
482F <482F.git.mail@482f.net>
2023-07-29 00:00:00 +0900

    sync 時にリモート用のブランチを作成するように

--- commands/sync.ts
+++ commands/sync.ts
@@ -6,7 +6,16 @@ async function syncAction() {
   const [shortStatus, shortStatusError] = await git.shortStatus()
   if (shortStatus !== '') {
     throw (shortStatusError ??
-      new ExpectedError('未コミットの変更が残っている状態で同期することはできません'))
+      new ExpectedError(
+        '未コミットの変更が残っている状態で同期することはできません',
+      ))
+  }
+
+  const [, createBranchError] = await git.createBranchIfNotExists(
+    'sync-config-remote',
+  )
+  if (createBranchError) {
+    throw createBranchError
   }
 }
 
