808e3812962317ffed167062bd2b47f60b0c6435
482F <482F.git.mail@482f.net>
2023-07-29 00:00:00 +0900

    git のブランチ名の持ち方を変更

--- commands/sync.ts
+++ commands/sync.ts
@@ -1,7 +1,7 @@
 import { Command } from 'https://deno.land/x/cliffy@v0.25.7/command/mod.ts'
 import { git } from '../utils/git.ts'
 import { ExpectedError } from '../utils/misc.ts'
-import { BranchNames } from '../utils/const.ts'
+import { Git } from '../utils/const.ts'
 
 async function prepareRemoteBranch() {
   const [, createBranchError] = await git.createBranchIfNotExists(
