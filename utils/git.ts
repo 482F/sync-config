4d4561d60ae7136a32c292354ff251e025d4dfa2
482F <482F.git.mail@482f.net>
2023-08-04 00:00:00 +0900

    checkout を連続して行える関数を実装し、既存の単発 checkout は内部でそれを利用するように

--- utils/git.ts
+++ utils/git.ts
@@ -1,4 +1,5 @@
-import { Result } from 'https://raw.githubusercontent.com/482F/482F-ts-utils/v2.x.x/src/result.ts'
+import type { IsNever } from 'https://raw.githubusercontent.com/482F/482F-ts-utils/v2.x.x/src/common.ts'
+import type { Result } from 'https://raw.githubusercontent.com/482F/482F-ts-utils/v2.x.x/src/result.ts'
 import { ExpectedError } from './misc.ts'
 
 async function callGit(args: string[]): Promise<Result<string>> {
@@ -9,6 +10,76 @@ async function callGit(args: string[]): Promise<Result<string>> {
     return [undefined, new Error(new TextDecoder().decode(output.stderr))]
   }
 }
+
+const sequentialCheckout = (() => {
+  function getObj<
+    const PrevCallbackReturnType,
+    const AllArgs extends readonly {
+      target: string
+      options: string[]
+      // deno-lint-ignore no-explicit-any
+      callback: (arg?: any) => OR
+    }[],
+    OR = unknown,
+  >(allArgs: AllArgs) {
+    return {
+      async do<
+        const RR extends {
+          [i in keyof AllArgs]: Awaited<ReturnType<AllArgs[i]['callback']>>
+        },
+        const R extends [RR, IsNever<OR>] extends [[], false] ? OR[] : RR,
+      >(): Promise<Result<R>> {
+        let r: Result<string>
+        r = await callGit(['branch', '--contains'])
+        if (r[1]) {
+          return [undefined, r[1]]
+        }
+
+        const originalName = r[0].match(
+          /^\* (\(HEAD detached at )?(.+?)\)?$/m,
+        )?.[2]
+
+        if (!originalName) {
+          return [
+            undefined,
+            new ExpectedError('現在のブランチ名の取得に失敗しました'),
+          ]
+        }
+
+        try {
+          const results: OR[] = []
+          for (const args of allArgs) {
+            r = await callGit([
+              'checkout',
+              ...(args.options ?? []),
+              args.target,
+            ])
+            if (r[1]) {
+              return [undefined, r[1]]
+            }
+
+            results.push(await args.callback(results.at(-1)))
+          }
+          return [results as R, undefined]
+        } catch (e) {
+          return [undefined, e]
+        } finally {
+          await callGit(['checkout', originalName])
+        }
+      },
+      add<const R extends OR>(
+        target: string,
+        options: string[],
+        callback: (arg: PrevCallbackReturnType) => R,
+      ) {
+        const nextAllArgs = [...allArgs, { target, options, callback }] as const
+        return getObj<R, typeof nextAllArgs>(nextAllArgs)
+      },
+    }
+  }
+  return <IR, OR = unknown>() => getObj<IR, [], OR>([])
+})()
+
 async function checkout<R>(
   target: string,
   options: string[],
@@ -23,33 +94,19 @@ async function checkout<R>(
   options?: string[],
   callback?: () => R,
 ): Promise<Result<undefined | Awaited<R>>> {
-  let r: Result<string>
-  r = await callGit(['branch', '--contains'])
-  if (r[1]) {
-    return [undefined, r[1]]
+  const seq = sequentialCheckout()
+  const [value, err] = await (() => {
+    if (callback) {
+      return seq.add(target, options ?? [], callback).do()
+    } else {
+      return seq.add(target, options ?? [], () => undefined).do()
+    }
+  })()
+  if (err) {
+    return [undefined, err]
   }
 
-  const originalName = r[0].match(/^\* (\(HEAD detached at )?(.+?)\)?$/m)?.[2]
-
-  if (!originalName) {
-    return [
-      undefined,
-      new ExpectedError('現在のブランチ名の取得に失敗しました'),
-    ]
-  }
-
-  r = await callGit(['checkout', ...(options ?? []), target])
-  if (r[1]) {
-    return [undefined, r[1]]
-  }
-
-  try {
-    return [await callback?.(), undefined]
-  } catch (e) {
-    return [undefined, e]
-  } finally {
-    await callGit(['checkout', originalName])
-  }
+  return [value[0], undefined]
 }
 
 export const git = {
@@ -157,10 +214,19 @@ export const git = {
 
     return [true, undefined]
   },
+  async execInBranches<R>(branches: string[], callback: () => R) {
+    return await branches
+      .reduce(
+        (seq, branch) => seq.add(branch, [], callback),
+        sequentialCheckout<unknown, R>(),
+      )
+      .do()
+  },
   checkout: async <R>(target: string, callback: () => R) =>
     await checkout(target, [], callback),
-} as const satisfies Record<
-  string,
-  // deno-lint-ignore no-explicit-any
-  (...args: any[]) => Promise<Result<unknown>>
->
+} as const satisfies {
+  [key: string]: (
+    // deno-lint-ignore no-explicit-any
+    ...args: any[]
+  ) => Promise<Result<unknown>>
+}
