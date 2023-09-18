5f9162ad54b73eb3f8034ceae3c6ef977b7b7f05
482F <482F.git.mail@482f.net>
2023-09-19 00:00:00 +0900

    ローカル用ブランチに関する記述を削除

--- utils/const.ts
+++ utils/const.ts
@@ -2,7 +2,6 @@ export const configFileName = 'sync-config.json5'
 
 export const git = {
   branch: {
-    local: 'sync-config---local',
     remote: 'sync-config---remote',
   },
   remote: 'sync-config---template-remote',
